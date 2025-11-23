4f625b0280b5fa2c7e24d6ae270bbecd21d6072f
benjacas <castro.ben26@gmail.com>
2025-11-23 20:46:57 -0300

    Agregar archivos de proyecto.

---
+++ pryPrueba/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace pryPrueba
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Card1_Tapped(object sender, EventArgs e)
+        {
+
+            var card = (Frame)sender;
+
+            // Animación de agrandado (zoom)
+            await card.ScaleTo(1.05, 100, Easing.CubicIn);   // 5% más grande
+            await card.ScaleTo(1.0, 100, Easing.CubicOut);   // vuelve al tamaño normal
+
+            // Acción después de la animación
+            await Navigation.PushAsync(new frmListaEjercicios());
+        }
+
+        private async void Card2_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new frmListaEjercicios());
+        }
+
+
+
+    }
+}
---
+++ pryPrueba/frmListaEjercicios.xaml.cs
@@ -0,0 +1,9 @@
+namespace pryPrueba;
+
+public partial class frmListaEjercicios : ContentPage
+{
+	public frmListaEjercicios()
+	{
+		InitializeComponent();
+	}
+}
